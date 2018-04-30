b246fee4f6984fe7fac872e352f9ad69ceac7f81
burger <36898041+Hassanish@users.noreply.github.com>
2018-04-30 18:32:01 -0500

    Update app.js

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,1 +1,46 @@
+$("#scrapeArticles").on("click", function(event) {
+    event.preventDefault();
+    $.ajax({
+        method: "GET",
+        url: "/scrape"
+    })
+    .done(function(data) {
+        console.log(data);
+        window.location = "/";
+    });
+});
 
+$("#homeButton").on("click", function(event) {
+    event.preventDefault();
+    window.location = "/"
+})
+
+
+$(document).on("click", ".saveArticle, .deleteArticle", function(event) {
+    event.preventDefault();
+    var id = $(this).data("id");
+    var saved = $(this).data("saved")
+
+    console.log(id);
+
+    $.ajax({
+        method: "PUT",
+        url: "/articles/" + id + "/" + saved
+    }).done(function(data) {
+        console.log(data);
+
+        if (data.saved) {
+            alert("This article has been saved");
+        }
+        else {
+            alert("This article has been deleted from your saved Articles.");
+            location.reload();
+        }
+    });
+
+  
+    $("#notesModal").on('shown.bs.modal', function (event) {
+        event.preventDefault();
+        $('#myInput').trigger('focus')
+      })
+});
