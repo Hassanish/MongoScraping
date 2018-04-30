743957512badf6b21ecccd76b01fd5dbf67ab240
burger <36898041+Hassanish@users.noreply.github.com>
2018-04-30 18:28:22 -0500

    Update Headline.js

--- models/Headline.js
+++ models/Headline.js
@@ -1,1 +1,31 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var HeadlineSchema = new Schema({
+
+  title: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  },
+ 
+  note: [{
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }]
+});
+
+var Headline = mongoose.model("Headline", HeadlineSchema);
+
+module.exports = Headline;
 
